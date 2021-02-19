20d2e3d85e339c9bd5e03beb0d1a36336320e2a8
tourdetravoy <dooganb@gmail.com>
2021-02-19 16:15:43 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# leaflet-map-polygon-hover
+Leaflet thematic polygon (choropleth) map, with hover info window, using GeoJSON data
+
+## Demo
+https://handsondataviz.github.io/leaflet-map-polygon-hover/
+
+## Credits
+Code improvements by [@ilyankou](https://github.com/ilyankou); code functions by [@alvinschang](https://github.com/alvinschang) Connecticut Mirror http://ctmirror.org
+
+## Create your own
+Learn more in the Leaflet map code template section of *Hands-On Data Visualization* book https://HandsOnDataViz.org/
---
+++ script.js
@@ -0,0 +1,142 @@
+// Edit the center point and zoom level
+var map = L.map('map', {
+  center: [10,-20],
+  zoom: 2,
+  zoomControl: false, // add later to reposition
+  scrollWheelZoom: false
+});
+
+// Edit links to your GitHub repo and data source credit
+map.attributionControl
+.setPrefix('View <a href="http://github.com/handsondataviz/leaflet-world-income-share">code and data on GitHub</a>, created with <a href="http://leafletjs.com" title="A JS library for interactive maps">Leaflet</a>');
+map.attributionControl.addAttribution('Income data &copy; <a href="https://wid.world/world/#sptinc_p99p100_z/US;FR;DE;CN;ZA;GB;WO/last/eu/k/p/yearly/s/false/5.070499999999999/30/curve/false/country">World Inequality Database</a>');
+
+// Basemap layer
+new L.tileLayer('http://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png', {
+attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
+}).addTo(map);
+
+// Reposition zoom control other than default topleft
+L.control.zoom({position: "topright"}).addTo(map);
+
+// Edit to upload GeoJSON data file from your local directory
+$.getJSON("map.geojson", function (data) {
+  geoJsonLayer = L.geoJson(data, {
+    style: style,
+    onEachFeature: onEachFeature
+  }).addTo(map);
+});
+
+// Edit ranges and colors to match your data; see http://colorbrewer.org
+// Any values not listed in the ranges below displays as the last color
+function getColor(d) {
+  return d > 20  ? '#d7301f' :
+         d > 10  ? '#fc8d59' :
+         d > 0.1 ? '#fdcc8a' :
+                   '#e6e6e6' ;
+}
+
+// Edit the getColor property to match data column header in your GeoJson file
+function style(feature) {
+  return {
+    fillColor: getColor(feature.properties.percent),
+    weight: 1,
+    opacity: 1,
+    color: 'black',
+    fillOpacity: 1
+  };
+}
+
+// This highlights the layer on hover, also for mobile
+function highlightFeature(e) {
+  resetHighlight(e);
+  var layer = e.target;
+  layer.setStyle({
+    weight: 4,
+    color: 'black',
+    fillOpacity: 0.9
+  });
+  info.update(layer.feature.properties);
+}
+
+// This resets the highlight after hover moves away
+function resetHighlight(e) {
+  geoJsonLayer.setStyle(style);
+  info.update();
+}
+
+// This instructs highlight and reset functions on hover movement
+function onEachFeature(feature, layer) {
+  layer.on({
+    mouseover: highlightFeature,
+    mouseout: resetHighlight,
+    click: highlightFeature
+  });
+}
+
+// Creates an info box on the map
+var info = L.control({position: 'topleft'});
+info.onAdd = function (map) {
+  this._div = L.DomUtil.create('div', 'info');
+  this.update();
+  return this._div;
+};
+
+// Edit info box text and variables (such as props.density2010) to match those in your GeoJSON data
+info.update = function (props) {
+  this._div.innerHTML = '<h3>Share of National Income by Top 1 Percent</h3>';
+
+  var value = props && props.percent ? props.percent + '%' : 'No data'
+
+  this._div.innerHTML +=  (props
+    ? '<b>' + props.name + '</b><br />' + value + '</b><br />'
+      + (props.year ? 'Most recent data: ' + props.year : '')
+    : 'Hover over nations');
+};
+info.addTo(map);
+
+// Edit grades in legend to match the ranges cutoffs inserted above
+// In this example, the last grade will appear as 5000+
+var legend = L.control({position: 'bottomleft'});
+legend.onAdd = function (map) {
+  var div = L.DomUtil.create('div', 'info legend'),
+    grades = [0, 10, 20],
+    labels = [],
+    from, to;
+  for (var i = 0; i < grades.length; i++) {
+    from = grades[i];
+    to = grades[i + 1];
+    labels.push(
+      '<i style="background:' + getColor(from + 1) + '"></i> ' +
+      from + (to ? '&ndash;' + to : '+') + '%');
+  }
+  div.innerHTML = labels.join('<br>');
+  return div;
+};
+legend.addTo(map);
+
+// Use in info.update if GeoJSON data contains null values, and if so, displays "--"
+function checkNull(val) {
+  if (val != null || val == "NaN") {
+    return comma(val);
+  } else {
+    return "--";
+  }
+}
+
+// Use in info.update if GeoJSON data needs to be displayed as a percentage
+function checkThePct(a,b) {
+  if (a != null && b != null) {
+    return Math.round(a/b*1000)/10 + "%";
+  } else {
+    return "--";
+  }
+}
+
+// Use in info.update if GeoJSON data needs to be displayed with commas (such as 123,456)
+function comma(val){
+  while (/(\d+)(\d{3})/.test(val.toString())){
+    val = val.toString().replace(/(\d+)(\d{3})/, '$1'+','+'$2');
+  }
+  return val;
+}
